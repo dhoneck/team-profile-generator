ae4958d2a4fda7ed1b30af3446a55cb692fb94f3
Dustin Honeck <13143062+dhoneck@users.noreply.github.com>
2023-02-08 15:09:55 -0600

    Move CSS to an intenal stylesheet

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,6 @@
 // Import library
 const fs = require('fs');
+const path = require('path');
 
 // Store html template
 const html =
@@ -10,8 +11,82 @@ const html =
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
   <title>Team Builder</title>
+  <style>
+  /* GENERAL */
+  :root {
+    --red: #E84856;
+    --blue: #0277F7;
+    --gray: #F6F7F9;
+  }
+  
+  body {
+    font-family: sans-serif;
+    margin: 0;
+  }
+  
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  
+  /* HEADER */
+  header {
+    background-color: var(--red);
+    color: white;
+    border-bottom: 2px solid black;
+    text-align: center;
+    height: 100px;
+    line-height: 100px;
+  }
+  
+  h1 {
+    margin: 0;
+  }
+  
+  /* MAIN CONTENT */
+  #member-container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    margin: 50px 20px 20px 20px;
+  }
+  
+  .member-tile {
+    flex-basis: 400px;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    box-shadow: 3px 3px 3px gray;
+  }
+  
+  .member-tile h2, .member-tile h3 {
+    margin: 0;
+  }
+  
+  .tile-top {
+    color: white;
+    background-color: var(--blue);
+    padding: 5px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+  }
+  
+  .tile-bottom {
+    background-color: var(--gray);
+    padding: 5px;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+  
+  table, td {
+    background-color: white;
+    padding: 3px;
+    border: 1px solid lightgray;
+    border-collapse: collapse;
+    width: 100%;
+  }
+  </style>
 </head>
 <body>
   <header>
@@ -24,98 +99,15 @@ const html =
 </html>
 `;
 
-// Store CSS template
-const css = 
-`
-/* GENERAL */
-:root {
-  --red: #E84856;
-  --blue: #0277F7;
-  --gray: #F6F7F9;
-}
-
-body {
-  font-family: sans-serif;
-  margin: 0;
-}
-
-ul {
-  list-style: none;
-  padding: 0;
-}
-
-/* HEADER */
-header {
-  background-color: var(--red);
-  color: white;
-  border-bottom: 2px solid black;
-  text-align: center;
-  height: 100px;
-  line-height: 100px;
-}
-
-h1 {
-  margin: 0;
-}
-
-/* MAIN CONTENT */
-#member-container {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 20px;
-  margin: 50px 20px 20px 20px;
-}
-
-.member-tile {
-  flex-basis: 400px;
-  border: 1px solid lightgray;
-  border-radius: 5px;
-  box-shadow: 3px 3px 3px gray;
-}
-
-.member-tile h2, .member-tile h3 {
-  margin: 0;
-}
-
-.tile-top {
-  color: white;
-  background-color: var(--blue);
-  padding: 5px;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-}
-
-.tile-bottom {
-  background-color: var(--gray);
-  padding: 5px;
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
-}
-
-table, td {
-  background-color: white;
-  padding: 3px;
-  border: 1px solid lightgray;
-  border-collapse: collapse;
-  width: 100%;
-}
-`;
-
-// Adds members to the html and writes the HTML and CSS files
+// Adds members to the html and write the html file
 function generateAndWriteFiles(data) {
   // Generate final html content
   let generatedHTML = generateHTML(data);
 
   // Write html
-  console.log('Attempting to write index.html and style.css');
+  console.log('Attempting to write index.html');
   fs.writeFile('index.html', generatedHTML, (err) =>
   err ? console.log(err) : console.log('index.html created successfully')
-  );
-  
-  // Write CSS
-  fs.writeFile('style.css', css, (err) =>
-  err ? console.log(err) : console.log('style.css created successfully')
   );
 }
 
